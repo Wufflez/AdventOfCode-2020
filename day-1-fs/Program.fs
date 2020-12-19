1ed1f985a8baf988d6f4eb291d1dbe70fc9dfd31
Adam Rhodes <wuffles236@gmail.com>
2020-12-19 18:03:06 +0000

    Complete day 1 F# version
    For some reason was just left half done

--- day-1-fs/Program.fs
+++ day-1-fs/Program.fs
@@ -1,29 +1,16 @@
-﻿let testData = [1;2;4;8;20]
+﻿let rec findSumTo sum n values =   
+    match values with
+        | first :: rest -> 
+            match n with 
+                | 2 -> match rest |> List.tryFind ((=) (sum - first)) with
+                        | Some other -> [first; other]
+                        | None -> findSumTo sum 2 rest
+                | _ -> match findSumTo (sum-first) (n-1) rest with
+                        | [] -> findSumTo sum n rest
+                        | others -> (first::others)                        
+        | _ -> []
 
-let rec findSumPair sum values = 
-    let first = values |> List.head
-    let rest = values |> list.head
-    match rest |> List.tryFind(fun x -> x = sum - first)
-        
-    
-let rec findSumTo sum n values =
-    let first = values |> List.head
-    let rest = values |> List.tail
-    match n with
-        | 2 ->  match rest |> List.tryFind((=) sum - first) with
-                    | Some(f) -> Some [first; f]
-                    | None -> None
-        | _ ->  findSumTo (sum-first) (n-1) rest
-       
- 
-//let findSumValues sum n values =
-//    match n with        
-//        | 2 -> for outer in values do
-//                   for inner in List.tail values do  
-//                        if outer + inner = sum then
-//                            Some [outer; inner;]  
-//                        else
-//                            None
+let input = System.IO.File.ReadAllLines "input.txt" |> Seq.map int |> Seq.toList
 
-printfn "Sum values = %A" (findSumTo 24 2 testData)
-    
+findSumTo 2020 2 input |> List.fold (*) 1 |> printfn "Part 1: %i"
+findSumTo 2020 3 input |> List.fold (*) 1 |> printfn "Part 2: %i"  

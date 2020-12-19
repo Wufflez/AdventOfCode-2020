ef581fa4fa064ef3560c3b145df97f53ec0e5642
Adam Rhodes <wuffles236@gmail.com>
2020-12-19 16:43:27 +0000

    Nice refactor of day 12 to single method

--- day-12-cs/Program.cs
+++ day-12-cs/Program.cs
@@ -5,45 +5,10 @@ using System.Linq;
 var instructions = File.ReadAllLines("input.txt")
     .Select(l => (l[0], int.Parse(l[1..]))).ToList();
 
-(int x, int y) Navigate()
-{
-    int angle = 0;
-    var position = (x: 0, y: 0);
-
-    void Move((int x, int y) vector)
-    {
-        position.x += vector.x;
-        position.y += vector.y;
-    }
-
-    foreach (var instruction in instructions)
-    {
-        switch (instruction)
-        {
-            case ('R', int amount): angle = (angle + amount + 360) % 360; break;
-            case ('L', int amount): angle = (angle - amount + 360) % 360; break;
-            case ('F', int distance):
-                Move(angle switch
-                {
-                    0 => (distance, 0),
-                    90 => (0, -distance),
-                    180 => (-distance, 0),
-                    270 => (0, distance),
-                });
-                break;
-            case ('N', int distance): Move((0, distance)); break;
-            case ('E', int distance): Move((distance, 0)); break;
-            case ('S', int distance): Move((0, -distance)); break;
-            case ('W', int distance): Move((-distance, 0)); break;
-        }
-    }
-    return position;
-}
-
-(int x, int y) NavigateWithWaypoint()
+(int x, int y) Navigate(bool useWaypoint)
 {
     var position = (x: 0, y: 0);
-    var waypoint = (x: 10, y: 1);
+    var waypoint = useWaypoint ? (x:10, y:1) : (x: 1, y: 0);
 
     void RotateWaypoint(int angle)
     {
@@ -55,6 +20,9 @@ var instructions = File.ReadAllLines("input.txt")
             270 => (-waypoint.y, waypoint.x),
         };
     }
+
+    ref var move = ref useWaypoint ? ref waypoint : ref position;
+
     foreach (var instruction in instructions)
     {
         switch (instruction)
@@ -65,17 +33,17 @@ var instructions = File.ReadAllLines("input.txt")
                 position.x += waypoint.x * times;
                 position.y += waypoint.y * times;
                 break;
-            case ('N', int distance): waypoint.y += distance; break;
-            case ('E', int distance): waypoint.x += distance; break;
-            case ('S', int distance): waypoint.y -= distance; break;
-            case ('W', int distance): waypoint.x -= distance; break;
+            case ('N', int distance): move.y += distance; break;
+            case ('E', int distance): move.x += distance; break;
+            case ('S', int distance): move.y -= distance; break;
+            case ('W', int distance): move.x -= distance; break;
         }
     }
     return position;
 }
 
-var partOnePos = Navigate();
+var partOnePos = Navigate(false);
 Console.WriteLine($"Part 1: {Math.Abs(partOnePos.x) + Math.Abs(partOnePos.y)}");
 
-var partTwoPos = NavigateWithWaypoint();
+var partTwoPos = Navigate(true);
 Console.WriteLine($"Part 2: {Math.Abs(partTwoPos.x) + Math.Abs(partTwoPos.y)}");

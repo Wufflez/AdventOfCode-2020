46b69a7b390a6fdda68841818db783e8a6dc85d9
Adam Rhodes <wuffles236@gmail.com>
2020-12-19 15:17:36 +0000

    Day 12 part 2

--- day-12-cs/Program.cs
+++ day-12-cs/Program.cs
@@ -5,36 +5,77 @@ using System.Linq;
 var instructions = File.ReadAllLines("input.txt")
     .Select(l => (l[0], int.Parse(l[1..]))).ToList();
 
-int angle = 0;
-var position = (x: 0, y: 0);
-
-void Move((int x, int y) vector)
+(int x, int y) Navigate()
 {
-    position.x += vector.x;
-    position.y += vector.y;
-}
+    int angle = 0;
+    var position = (x: 0, y: 0);
 
-void Turn(int degrees) => angle = (angle + degrees + 360) % 360;
-foreach (var instruction in instructions)
-{
-    switch (instruction)
+    void Move((int x, int y) vector)
     {
-        case ('R', int amount): angle = (angle + amount + 360) % 360; break;
-        case ('L', int amount): angle = (angle - amount + 360) % 360; break;
-        case ('F', int distance):
-            Move(angle switch
-            {
-                0 => (distance, 0),
-                90 => (0, -distance),
-                180 => (-distance, 0),
-                270 => (0, distance),
-            });
-            break;
-        case ('N', int distance): Move((0, distance)); break;
-        case ('E', int distance): Move((distance, 0)); break;
-        case ('S', int distance): Move((0, -distance)); break;
-        case ('W', int distance): Move((-distance, 0)); break;
+        position.x += vector.x;
+        position.y += vector.y;
     }
+
+    foreach (var instruction in instructions)
+    {
+        switch (instruction)
+        {
+            case ('R', int amount): angle = (angle + amount + 360) % 360; break;
+            case ('L', int amount): angle = (angle - amount + 360) % 360; break;
+            case ('F', int distance):
+                Move(angle switch
+                {
+                    0 => (distance, 0),
+                    90 => (0, -distance),
+                    180 => (-distance, 0),
+                    270 => (0, distance),
+                });
+                break;
+            case ('N', int distance): Move((0, distance)); break;
+            case ('E', int distance): Move((distance, 0)); break;
+            case ('S', int distance): Move((0, -distance)); break;
+            case ('W', int distance): Move((-distance, 0)); break;
+        }
+    }
+    return position;
 }
 
-Console.WriteLine($"Part 1: {Math.Abs(position.x) + Math.Abs(position.y)}");
+(int x, int y) NavigateWithWaypoint()
+{
+    var position = (x: 0, y: 0);
+    var waypoint = (x: 10, y: 1);
+
+    void RotateWaypoint(int angle)
+    {
+        waypoint = ((angle + 360) % 360) switch
+        {
+            0 => waypoint,
+            90 => (waypoint.y, -waypoint.x),
+            180 => (-waypoint.x, -waypoint.y),
+            270 => (-waypoint.y, waypoint.x),
+        };
+    }
+    foreach (var instruction in instructions)
+    {
+        switch (instruction)
+        {
+            case ('R', int angle): RotateWaypoint(angle); break;
+            case ('L', int angle): RotateWaypoint(-angle); break;
+            case ('F', int times):
+                position.x += waypoint.x * times;
+                position.y += waypoint.y * times;
+                break;
+            case ('N', int distance): waypoint.y += distance; break;
+            case ('E', int distance): waypoint.x += distance; break;
+            case ('S', int distance): waypoint.y -= distance; break;
+            case ('W', int distance): waypoint.x -= distance; break;
+        }
+    }
+    return position;
+}
+
+var partOnePos = Navigate();
+Console.WriteLine($"Part 1: {Math.Abs(partOnePos.x) + Math.Abs(partOnePos.y)}");
+
+var partTwoPos = NavigateWithWaypoint();
+Console.WriteLine($"Part 2: {Math.Abs(partTwoPos.x) + Math.Abs(partTwoPos.y)}");

13147561c4a27ef2945d3c8c1c36372c18be1934
Adam Rhodes <wuffles236@gmail.com>
2020-12-25 16:50:51 +0000

    Simpler and more optimal solver for Day 15

--- day-15-cs/Program.cs
+++ day-15-cs/Program.cs
@@ -1,27 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-static IEnumerable<int> GuessingGame(params int[] startNums)
+static int GuessingGame(int nth, params int[] startNums)
 {
     int position = 0;
     int lastNumber = startNums.Last();
-    var positions = new Dictionary<int, int>();
-
+    var positions = new int[nth];
     foreach (var number in startNums)
-    {
         positions[number] = ++position;
-        yield return number;
-    }
 
-    while (true)
+    while (position < nth)
     {
-        int nextNumber = positions.TryGetValue(lastNumber, out int lastSeenPosition) ? position - lastSeenPosition : 0;
-        yield return nextNumber;
+        int lastPosition = positions[lastNumber];
+        int nextNumber = lastPosition != 0 ? position - lastPosition : 0;
         positions[lastNumber] = position++;
         lastNumber = nextNumber;
     }
+
+    return lastNumber;
 }
 
-Console.WriteLine("Part 1: " + GuessingGame(0, 3, 1, 6, 7, 5).Take(2020).Last());
-Console.WriteLine("Part 2: " + GuessingGame(0, 3, 1, 6, 7, 5).Take(30000000).Last());
+Console.WriteLine("Part 1: " + GuessingGame(2020, 0, 3, 1, 6, 7, 5));
+Console.WriteLine("Part 2: " + GuessingGame(30000000, 0, 3, 1, 6, 7, 5));
